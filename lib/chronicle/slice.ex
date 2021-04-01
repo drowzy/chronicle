4c5b726d062a3b58b3b440de358fea3d1de4b335
Simon Thörnqvist <simon.thornqvist@gmail.com>
2021-04-01 16:47:58 +0200

    fix! offsets are based on total offset in file naming

--- lib/chronicle/slice.ex
+++ lib/chronicle/slice.ex
@@ -5,14 +5,22 @@ defmodule Chronicle.Slice do
 
   @type fd :: charlist()
   @type t :: %__MODULE__{}
+  @type options :: [
+          max_size: integer(),
+          path: Path.t(),
+          offset: integer(),
+          fd_write: (term(), term() -> term())
+        ]
+
   defstruct [
     :fd,
     :log,
     :index,
+    :offset,
     :opts
   ]
 
-  @spec open(Keyword.t()) :: term()
+  @spec open(options()) :: term()
   def open(opts) do
     opts = Options.sanitize(opts)
     dl_opts = Options.dlog_options(opts)
@@ -25,18 +33,21 @@ defmodule Chronicle.Slice do
        %__MODULE__{
          log: log,
          fd: fd,
+         offset: opts.offset,
          opts: opts
        }}
     end
   end
 
   @spec write(t(), iodata()) :: {:ok, t()} | {:error, term()}
-  def write(%__MODULE__{log: log} = slice, bin) do
+  def write(slice, bin) when is_binary(bin), do: write(slice, [bin])
+
+  def write(%__MODULE__{log: log, offset: offset} = slice, bin) do
     case Log.write(log, bin) do
       {:ok, log} ->
-        {:ok, %{slice | log: log}}
+        {:ok, %{slice | log: log, offset: offset + 1}}
 
-      {:error, %Error{reason: {:nospace, _, _, _}}} ->
+      {:error, %Error{reason: {:enospc, _, _, _}}} ->
         slice
         |> rotate_log()
         |> write(bin)
@@ -44,10 +55,10 @@ defmodule Chronicle.Slice do
   end
 
   @spec rotate_log(t()) :: t()
-  def rotate_log(%__MODULE__{opts: opts} = slice) do
+  def rotate_log(%__MODULE__{opts: opts, offset: offset} = slice) do
     args = [
-      offset: opts.offset + 1,
-      size: opts.max_size,
+      offset: offset + 1,
+      max_size: opts.max_size,
       path: opts.base_path
     ]
 
